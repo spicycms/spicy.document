b0bfcda173a2c98f31d79438ad1f74459cb38a14
Alexander Burtsev <eburus@gmail.com>
2017-03-14 15:02:24 +0300

    Clean unused code and ckeditor

--- src/spicy/document/urls.py
+++ src/spicy/document/urls.py
@@ -20,7 +20,6 @@ for category in defaults.RSS_CATEGORIES:
 public_urls += patterns(
     'spicy.document.views',
     url(r'^a(?P<doc_id>\d+)/$', 'document', name='doc'),
-    #url(r'^create/$', 'create_article', name='create'),
 )
 
 admin_urls = patterns(
@@ -32,8 +31,6 @@ admin_urls = patterns(
 
     url(r'^create/$', 'create', name='create'),
     url(r'^edit/(?P<doc_id>\d+)/$', 'edit', name='edit'),
-    #url(r'^edit/(?P<doc_id>\d+)/comments/$', 'edit_comments',
-    #    name='edit-comments'),
     url(r'^(?P<doc_id>\d+)/delete/$', 'delete', name='delete'),
     #url(r'^edit/(?P<doc_id>\d+)/history/$', 'edit_doc_history', name='edit-doc-history'),
 )
